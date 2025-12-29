445bd36531088afaaaf815deb79bab768e57846c
omraval18 <r.auckwell18@gmail.com>
2025-12-29 22:56:19 +0530

    fix: drizzle neon serverless connection

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,3 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from "@neondatabase/serverless";
+import { env } from '../../env.js';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const sql = neon(env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
